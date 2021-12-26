43428424d53f93d140e536b793c0e7c23e9c23d6
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-12-26 10:39:45 +0900

    reformat

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,7 +5,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
     Ok(views.html.index("Hello"))
